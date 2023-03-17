919eb93cb3824a39848925fb8a5ef524c7f67810
VadymMiezin <115612677+VadymMiezin@users.noreply.github.com>
2023-03-17 14:52:22 +0000

    Deploying to gh-pages from @ VadymMiezin/goit-js-hw-09@dbbd0f5a143a8c8f4f5bf963a753670d21d87ab9 🚀

---
+++ 01-color-switcher.5006fc75.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");n.disabled=!0;var a=null;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.5006fc75.js.map
---
+++ 01-color-switcher.aece52d1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");d.disabled=!0;let a=null;t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,a=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.aece52d1.js.map
